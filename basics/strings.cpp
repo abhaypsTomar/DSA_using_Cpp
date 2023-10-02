9c2ade0a7c55a4dcdc59588efdd89106847b6922
abhaypsTomar <abhaypratap20315@gmail.com>
2023-10-02 22:35:25 +0530

    new

---
+++ basics/strings.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+int main(){
+	
+	string str = "I love her so much <3 churu";
+	string str2 = "nolive" ;
+	
+	cout << str.substr(2,4);
+	
+	
+//	if(str.find(str2)== string::npos){
+//		cout << "Not Found" << endl;
+//	}
+//	else{
+//		cout << "Found" << endl ;
+//	}
+	
+//	string name = "Muskaan Dudi";
+//	name.erase(name.begin()+7,name.end());
+//	cout << name << endl;
+//	string lastName = "Tomar";
+//	name.insert(7,lastName);
+//	cout << name ;
+	
+//	string sent = "this is a car";
+//	
+//	sent.erase(6,4);
+//	cout << sent << endl;
+//	string str1 = "Abhay";
+//	string str2 = "weds";
+//	string str3 = "Muskaan";
+//	
+//	cout << str1.append(str2) << str3 << endl;
+//	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		//creating
+	//	string name;
+	//	
+	//	cin >> name;
+	//	
+	//	cout << name  << " " << endl;
+	//	
+	//	string temp = "";
+	//	
+	////	cout <<"length" << name.length()<<endl;
+	////	cout <<"empty" << temp.empty()<<endl;
+	////	cout <<"length" << name.size()<<endl;
+	////	cout <<"at string : " << name.at(0)<<endl;
+	////	cout << "front char : " << name.front() << endl;
+	//	cout << "back char : "<< name.back() << endl;
+	//	//input
+	////	getline(cin,name);
+	////	
+	//	int index = 0;
+	//	while(name[index]!='\0'){
+	//		cout << index << " " << name[index] << endl;
+	//		index++;
+	//	}
+		
+		//out
+	//	cout << name;
+			
+		
+}
