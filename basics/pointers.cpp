dfde75243fd04bd8b975b9c680e15242d6b2c010
abhaypsTomar <abhaypratap20315@gmail.com>
2023-10-10 22:57:02 +0530

    update

---
+++ basics/pointers.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	char ch[10] = "love";
+	char * cptr = ch;
+	
+	cout << ch << endl;
+	cout << &ch << endl;
+	cout << ch[0] << endl;
+	cout << cptr << endl;
+	cout << *cptr << endl;
+	
+//	int arr[10] = {10,20,30,40,50};
+//	
+//	int *p =arr;
+//	
+//	int *q = arr+1;
+//	
+//	cout << *p << endl;
+//	cout << *q << endl;
+//	cout << *(p+1) <<endl;
+//	cout << *(p)+1 << endl;
+//	cout << *(q+5)<< endl;
+//	
+	
+//	int a = 5;
+//	
+//	int *p = &a;
+//	
+//	cout << p << endl;
+//	p=p+1;
+//	
+//	cout << p << endl; 
+//	
+//	int arr[10] = {10,20,30,40,50};
+//	arr = arr+1;
+//	cout << arr << endl;
+//	cout << &arr << endl;
+//	cout << &arr[0] << endl;
+//	cout << *arr+1 << endl;
+//	cout << *(arr)<< endl;
+//	cout << *(arr)+1<< endl;
+//	cout << *(arr+1)<< endl;
+//	cout << *(arr+2)<< endl;
+
+
+
+	
+	
+//	int a = 10;
+//	
+//	int *ptr = &a;
+//	int *q = ptr;
+//	
+//	cout<<a<<endl;
+//	cout << *ptr<<endl;
+//	cout << *q <<endl;
+//	cout << q <<endl;
+//	cout << &q << endl;
+
+	
+//	int a =10;
+//	
+//	int*  ptr = &a ;
+//	
+//	cout << sizeof(*ptr);
+}
